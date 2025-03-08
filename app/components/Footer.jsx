f3ff109601fa70a31645c091875bab6a2bff5937
fakhri-iz <fakhriikhwnauzaki@gmail.com>
2025-03-09 01:25:32 +0700

    add layout

---
+++ app/components/Footer.jsx
@@ -0,0 +1,55 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Footer = ({ isDarkMode }) => {
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image
+          src={isDarkMode ? assets.logo_dark : assets.logo}
+          alt=""
+          className="w-36 mx-auto mb-2"
+        />
+
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image
+            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
+            alt=""
+            className="w-6"
+          />
+          fakhrizaki27@gmail.com
+        </div>
+      </div>
+
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>© 2025 Fakhri. All rights reserved.</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/fakhri-iz/">
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/muhammad-fakhri-ikhwanuzaki/"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/fakhri_iz?igsh=MTBneGxrOHc5azFvNA=="
+            >
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/components/Header.jsx
@@ -0,0 +1,60 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+import { motion } from "motion/react";
+
+const Header = () => {
+  return (
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-3">
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+      >
+        <Image src={assets.profile_fakhri} alt="" className="rounded w-32" />
+      </motion.div>
+      {/* <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-2 font-Ovo">
+        Hi! <Image src={assets.hand_icon} alt="" className="w-6" />
+      </h3> */}
+      <motion.h1
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.3 }}
+        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
+      >
+        Hi! I'm Fakhri
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        className="max-w-2xl mx-auto font-Ovo"
+      >
+        Welcome to my portfolio. Here you’ll find a selection of my work.
+        Explore my projects to learn more about what I do.
+      </motion.p>
+
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-2">
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
+        >
+          Contact Me
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </motion.a>
+        {/* <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+        >
+          My Resume <Image src={assets.download_icon} alt="" className="w-4" />
+        </a> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
