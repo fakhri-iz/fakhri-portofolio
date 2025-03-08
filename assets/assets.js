66ed040b8f17b4576366f49973aadb43be324dfb
fakhri-iz <fakhriikhwnauzaki@gmail.com>
2025-03-09 00:42:41 +0700

    first commit

---
+++ assets/assets.js
@@ -0,0 +1,185 @@
+import user_image from "./user-image.png";
+import user_fakhri from "./user-fakhri.jpg";
+import code_icon from "./code-icon.png";
+import code_icon_dark from "./code-icon-dark.png";
+import edu_icon from "./edu-icon.png";
+import edu_icon_dark from "./edu-icon-dark.png";
+import project_icon from "./project-icon.png";
+import project_icon_dark from "./project-icon-dark.png";
+import vscode from "./vscode.png";
+import firebase from "./firebase.png";
+import figma from "./figma.png";
+import git from "./git.png";
+import mongodb from "./mongodb.png";
+import right_arrow_white from "./right-arrow-white.png";
+import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
+import mail_icon from "./mail_icon.png";
+import mail_icon_dark from "./mail_icon_dark.png";
+import profile_img from "./profile-img.png";
+import profile_fakhri from "./profil-fakhri.png";
+import download_icon from "./download-icon.png";
+import hand_icon from "./hand-icon.png";
+import header_bg_color from "./header-bg-color.png";
+import moon_icon from "./moon_icon.png";
+import sun_icon from "./sun_icon.png";
+import arrow_icon from "./arrow-icon.png";
+import arrow_icon_dark from "./arrow-icon-dark.png";
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import web_icon from "./web-icon.png";
+import mobile_icon from "./mobile-icon.png";
+import ui_icon from "./ui-icon.png";
+import graphics_icon from "./graphics-icon.png";
+import right_arrow from "./right-arrow.png";
+import send_icon from "./send-icon.png";
+import right_arrow_bold from "./right-arrow-bold.png";
+import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
+import laravel from "./laravel.png";
+import html from "./html.png";
+import css from "./css.png";
+import php from "./php.png";
+import js from "./js.png";
+import react from "./react.png";
+import tailwind from "./tailwind.png";
+import mysql from "./mysql.png";
+import code from "./code.svg";
+import cube from "./3dcube.svg";
+
+export const assets = {
+  user_image,
+  user_fakhri,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  project_icon,
+  project_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  mongodb,
+  right_arrow_white,
+  logo,
+  logo_dark,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  profile_fakhri,
+  download_icon,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  web_icon,
+  mobile_icon,
+  ui_icon,
+  graphics_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
+  laravel,
+  html,
+  css,
+  react,
+  php,
+  js,
+  tailwind,
+  mysql,
+  code,
+  cube,
+};
+
+export const workData = [
+  {
+    title: "Web Course LMS",
+    link: "https://github.com/fakhri-iz/MERN-LMS",
+    bgImage: "/work-1.png",
+  },
+  {
+    title: "Creative Agency Portfolio",
+    link: "https://github.com/fakhri-iz/creatifolio",
+    bgImage: "/work-2.png",
+  },
+  {
+    title: "Web Booking Flight Tickets",
+    link: "https://github.com/fakhri-iz/whoosh-flight",
+    bgImage: "/work-3.png",
+  },
+  {
+    title: "Shujia Home Service",
+    link: "https://github.com/fakhri-iz/homeservicefe",
+    bgImage: "/work-4.png",
+  },
+];
+
+export const serviceData = [
+  {
+    icon: assets.web_icon,
+    title: "Web Development",
+    description:
+      "Delivering great experience to users so that they are comfortable while using your product to grow.",
+    link: "",
+  },
+  {
+    icon: assets.graphics_icon,
+    title: "Frontend Development",
+    description:
+      "Delivering great experience to users so that they are comfortable while using your product to grow.",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "Backend Development",
+    description:
+      "Delivering great experience to users so that they are comfortable while using your product to grow.",
+    link: "",
+  },
+];
+
+export const infoList = [
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: "Languages",
+    description: "PHP & JavaScript",
+  },
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: "Education",
+    description: "B.Tech in Informatics Engineering",
+  },
+  {
+    icon: assets.project_icon,
+    iconDark: assets.project_icon_dark,
+    title: "Projects",
+    description: "Built more than 3 projects",
+  },
+];
+
+export const toolsData = [
+  assets.html,
+  assets.css,
+  assets.js,
+  assets.php,
+  assets.laravel,
+  assets.react,
+  assets.tailwind,
+  assets.mongodb,
+  assets.mysql,
+  assets.git,
+  // assets.vscode,
+  // assets.firebase,
+  // assets.figma,
+];
